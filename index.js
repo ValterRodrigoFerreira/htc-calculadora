64604c82eca33bb392311e69b03c06053c0737bf
Valter Rodrigo Ferreira <101230962+ValterRodrigoFerreira@users.noreply.github.com>
2022-05-11 22:19:20 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const $buttonsOperators = document.querySelectorAll(
 );
 const $buttonResult = document.querySelector(".calculator__buttons--result");
 
-const $buttonClear = document.querySelector(".calculator__buttons--clear");
+const $buttonClean = document.querySelector(".calculator__buttons--clean");
 
 const $inpuntDisplayCalculator = document.querySelector(
   ".calculator__display-input"
@@ -14,7 +14,9 @@ const $inpuntDisplayCalculator = document.querySelector(
 
 const $result = document.querySelector(".calculator__display-result");
 
-let calculate = "";
+const $changeSign = document.querySelector(".calculator__buttons--moreless");
+
+const inverterSinal = "-";
 
 $buttonsNumbers.forEach(function ($button) {
   $button.addEventListener("click", function () {
@@ -31,70 +33,20 @@ $buttonsOperators.forEach(function ($button) {
 });
 
 $buttonResult.addEventListener("click", function () {
+  calculate = calculate.replaceAll("x", "*");
+  calculate = calculate.replaceAll("÷", "/");
   $result.textContent = eval(calculate);
 });
 
-/*$buttonClear.addEventListener("click", function() {
-  $buttonResult.textContent = cl })*/
-
-/*const $buttonsNumber0 = document.querySelector(".calculator__buttons--number0");
-const $buttonsNumber1 = document.querySelector(".calculator__buttons--number1");
-const $buttonsNumber2 = document.querySelector(".calculator__buttons--number2");
-const $buttonsNumber3 = document.querySelector(".calculator__buttons--number3");
-const $buttonsNumber4 = document.querySelector(".calculator__buttons--number4");
-const $buttonsNumber5 = document.querySelector(".calculator__buttons--number5");
-const $buttonsNumber6 = document.querySelector(".calculator__buttons--number6");
-const $buttonsNumber7 = document.querySelector(".calculator__buttons--number7");
-const $buttonsNumber8 = document.querySelector(".calculator__buttons--number8");
-const $buttonsNumber9 = document.querySelector(".calculator__buttons--number9");
-
-const $inpuntDisplayCalculator = document.querySelector(
-  ".calculator__display-input"
-);
-
-const $boardListNumbers = document.querySelectorAll(
-  ".calculator__buttons--button"
-);
-
-let calculate = "";
-$inpuntDisplayCalculator.value = calculate;
-
-$buttonsNumber0.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $boardListNumbers.textContent;
+$buttonClean.addEventListener("click", function () {
+  $inpuntDisplayCalculator.value = "";
+  $result.textContent = "";
+  calculate = "";
 });
 
-$buttonsNumber1.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber1.textContent;
+$changeSign.addEventListener("click", function () {
+  $inpuntDisplayCalculator.value =
+    $inpuntDisplayCalculator.value != "-"
+      ? "-" + $inpuntDisplayCalculator.value
+      : ($inpuntDisplayCalculator.value = "+");
 });
-
-$buttonsNumber2.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber2.textContent;
-});
-
-$buttonsNumber3.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber3.textContent;
-});
-
-$buttonsNumber4.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber4.textContent;
-});
-
-$buttonsNumber5.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber5.textContent;
-});
-
-$buttonsNumber6.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber6.textContent;
-});
-
-$buttonsNumber7.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber7.textContent;
-});
-
-$buttonsNumber8.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber8.textContent;
-});
-
-$buttonsNumber9.addEventListener("click", function () {
-  $inpuntDisplayCalculator.value = $buttonsNumber9.textContent;
-});*/
